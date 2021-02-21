61289791ea54e5cdc327c7dd4e8da80060022b9c
Markus Engsner <markus.engsner@gmail.com>
2021-02-21 18:04:24 +0100

    initial commit

---
+++ Core/User/Examples/EPD_2in9_test.h
@@ -0,0 +1,10 @@
+//
+// Created by markus on 2021-02-21.
+//
+
+#ifndef CPPFROMBLINKY_EPD_2IN9_TEST_H
+#define CPPFROMBLINKY_EPD_2IN9_TEST_H
+
+int EPD_2in9_test(void);
+
+#endif //CPPFROMBLINKY_EPD_2IN9_TEST_H
